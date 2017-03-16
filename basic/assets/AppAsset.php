fa5315642b52f47e7f970428994b8bd7c2461e27
Darya Kiryanova <bella21abyss@gmail.com>
2017-03-16 23:04:28 +0300

    Moving basic css to another folder

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/style.css'
+        'basic/site.css'
     ];
     public $js = [
     ];
